81447843b4f27de682a4ed35209ccce554c4a377
Dominik Matras <matrasss02@gmail.com>
2024-02-09 19:03:12 +0100

    Done, final.

--- Laboratoruim2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Laboratoruim2.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
